c22996c5fef74887a31d4d10de01a5de71e3c019
polymatter <jlake.jobs@gmail.com>
2013-04-13 15:43:17 +0100

    display the whole year. use lucius to style

--- Handler/Reminder.hs
+++ Handler/Reminder.hs
@@ -5,4 +5,5 @@ import Import
 
 -- defaultLayout :: Yesod a => GWidget sub a () -> GHandler sub a RepHtml
 getReminderR :: Handler RepHtml
-getReminderR = do defaultLayout $ do $(widgetFile "yearview")
+getReminderR = do
+  defaultLayout $ do $(widgetFile "yearview")
