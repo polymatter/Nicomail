d9408a37eb892037d79f0907f7812f5d152f3a38
polymatter <jlake.jobs@gmail.com>
2013-04-13 13:27:46 +0100

    manually added in a new handler to display reminders. Next time, use 'yesod add-handler'. Its much more simpler and I'm not sure I did anything more than waste time doing it all manually

---
+++ Handler/Reminder.hs
@@ -0,0 +1,8 @@
+{-# LANGUAGE TupleSections, OverloadedStrings #-}
+module Handler.Reminder where
+
+import Import
+
+-- defaultLayout :: Yesod a => GWidget sub a () -> GHandler sub a RepHtml
+getReminderR :: Handler RepHtml
+getReminderR = do defaultLayout $ do $(widgetFile "yearview")
